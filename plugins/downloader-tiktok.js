b9dc28bb67303e08948def3517d37059fbea68ff
ShooC <118227993+ShooC@users.noreply.github.com>
2022-11-16 13:02:54 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,13 +1,20 @@
-const hxz = require("hxz-api")
-let handler = async(m,{text, conn}) => {
-
-let p = await  hxz.ttdownloader(text)
-const { nowm, wm, audio } = p
-conn.sendFile(m.chat, nowm, null, 'Nih kak (⁠≧⁠▽⁠≦⁠)', m)
+let fetch = require('node-fetch')
+let handler = async (m, { conn, args }) => {
+if (!args[0]) throw 'Berikan link tiktok'
+m.reply('tunggu')
+let res = await fetch(`https://botcahx.ddns.net/api/dowloader/tikok?url=${args[0]}`)
+if (!res.ok) throw await res.text()
+let json = await res.json()
+if (!json.status) throw json
+let { video, description, username } = json.result
+await conn.sendFile(m.chat, video, 'tiktok.mp4', `
+\n📜 *Deskripsi*: ${description}\n\n👾 *Username*: ${username}\n\n📮 *By*: Slime Bot
+`, m, false, { contextInfo: { forwardingScore: 999, isForwarded: false }})
 }
-handler.help = ['tiktok'].map(v => v + ' <url>')
+
+handler.help = ['tiktok <url>']
 handler.tags = ['downloader']
-handler.command = /^(tiktok)$/i
-handler.private = true
+handler.command = /^(tt|tiktok)$/i
 handler.limit = true
+
 module.exports = handler
