dc62f9e45e25648aab51d6983a62232ccfa6151c
ShooC <118227993+ShooC@users.noreply.github.com>
2022-11-18 16:18:39 +0700

    Update downloader-ytv.js

--- plugins/downloader-ytv.js
+++ plugins/downloader-ytv.js
@@ -1,45 +1,25 @@
-let limit = 1024
+let limit = 50
 let fetch = require('node-fetch')
 const { servers, ytv } = require('../lib/y2mate')
-let handler = async (m, { conn, args, isPrems, isOwner }) => {
-  if (!args || !args[0]) throw 'Uhm... urlnya mana bang?'
-  let chat = global.db.data.chats[m.chat]
-  let server = (args[1] || servers[0]).toLowerCase()
-  let { dl_link, thumb, title, filesize, filesizeF} = await ytv(args[0], servers.includes(server) ? server : servers[0])
-  let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
-  let name = await conn.getName(m.sender)
-let wm = global.wm
-  conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
-*Title:* ${title}
-*Filesize:* ${filesizeF}
-*${isLimit ? 'Pakai ': ''}Link:* ${dl_link}
-`.trim(), m)
-  let _thumb = {}
-  try { _thumb = { thumbnail: await (await fetch(thumb)).buffer() } }
-  catch (e) { }
-  if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp4', `
-*Title:* ${title}
-*Filesize:* ${filesizeF}
-`.trim(), m, false, {
-  ..._thumb,
-  asDocument: chat.useDocument
-})
+let handler = async(m, { conn, args, isPrems, isOwner }) => {
+    if (!args || !args[0]) return conn.reply(m.chat, 'Uhm... urlnya mana?', m)
+    let chat = global.db.data.chats[m.chat]
+    let server = (args[1] || servers[0]).toLowerCase()
+    let { dl_link, thumb, title, filesize, filesizeF } = await ytv(args[0], servers.includes(server) ? server : servers[0])
+    let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
+    conn.reply(isLimit ? `Ukuran File: ${filesizeF}\nUkuran file diatas ${limit} MB, download sendiri: ${dl_link}` : global.wait, m)
+    let _thumb = {}
+    try { _thumb = { thumbnail: await (await fetch(thumb)).buffer() } } catch (e) {}
+    m.reply(wait)
+    if (!isLimit) await conn.sendButtonVid(m.chat, dl_link, `*Title:* ${title}\n*Filesize:* ${filesizeF}`.trim(), wm, 'Audio', '.tomp3', m)
+	//await conn.sendMessage(m.chat, { document: { url: dl_link }, mimetype: 'video/mp4', fileName: title + `.mp4`}, {quoted: m})
+//conn.sendFile(m.chat, dl_link, title + '.mp4', `
+//*Title:* ${title}
+//*Filesize:* ${filesizeF}
+//   `.trim(), m, false, { thumbnail: Buffer.alloc(0), mimetype: 'video/mp4' })
 }
-handler.help = ['mp4','v',''].map(v => 'yt' + v + ` <url>`)
+handler.help = ['ytmp4 <query>']
 handler.tags = ['downloader']
-handler.command = /^yt(v|mp4)?$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-handler.private = true
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
-handler.exp = 0
+handler.command = /^yt(v(idi?e?o)?|mp4)?$/i
 handler.limit = true
-
 module.exports = handler
-
